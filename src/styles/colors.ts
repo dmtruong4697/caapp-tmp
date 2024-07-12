982a38107e84996e147bb0afb006c278a255c26b
Dương Minh Trưởng <duongminhtruong@Vinicorps-iMac.local>
2024-07-12 13:54:39 +0700

    add app color, radius

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,3 +1,16 @@
 export const colors = {
+    // background color
+    DefaultBackground: '#FFFFFF',
+    GrayBackground: '##d4d4d4',
+
+    // color
+    White: "#FFFFFF",
+    Black: "#000000",
+
+    // text color
+    PrimaryText: '#111111',
+    SecondText: '#666666',
     
+    // border color
+    GrayBorder: '##bdbdbd',
 }
